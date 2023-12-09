25b181d975386affc2d3e6c82d1de4c6746d0170
Mavrick Laakso <laakso.mavrick@gmail.com>
2023-12-09 18:07:54 -0500

    Mapped user_id in ratings to be a uuid

--- model/data/normalize_data_script.py
+++ model/data/normalize_data_script.py
@@ -8,16 +8,24 @@ def start():
     shows_df = read_csv(join(project_root, "data/imdb_shows.csv"))
     ratings_df = read_csv(join(project_root, "data/imdb_ratings.csv"))
 
-    id_mapping = {}
+    show_id_mapping = {}
+    user_id_mapping = {}
 
     for df in [shows_df, ratings_df]:
         for index, row in df.iterrows():
-            old_id = row["show_id"]
-            if old_id not in id_mapping:
-                id_mapping[old_id] = str(uuid4())
+            old_show_id = row["show_id"]
+            if old_show_id not in show_id_mapping:
+                show_id_mapping[old_show_id] = str(uuid4())
+
+    for index, row in ratings_df.iterrows():
+        old_user_id = row["user_id"]
+        if old_user_id not in user_id_mapping:
+            user_id_mapping[old_user_id] = str(uuid4())
 
     for df in [shows_df, ratings_df]:
-        df["show_id"] = df["show_id"].map(id_mapping)
+        df["show_id"] = df["show_id"].map(show_id_mapping)
+
+    ratings_df["user_id"] = ratings_df["user_id"].map(user_id_mapping)
 
     shows_df.to_csv(join(project_root, "data/shows.csv"), index=False)
     ratings_df.to_csv(join(project_root, "data/ratings.csv"), index=False)
